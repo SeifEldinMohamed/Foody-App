86d66fdaa1913f0706814cd7bca9867bfb04fa62
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-03-22 20:21:03 +0200

    nav graph + bottom navigation bar added

--- app/src/main/java/com/seif/foody/MainActivity.kt
+++ app/src/main/java/com/seif/foody/MainActivity.kt
@@ -2,10 +2,37 @@ package com.seif.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.seif.foody.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipeFragment,
+            R.id.favouriteRecipeFragment,
+            R.id.foodJokeFragment,
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+
 }
