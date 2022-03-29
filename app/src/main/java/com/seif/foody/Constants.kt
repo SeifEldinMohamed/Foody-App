a6d1b9dc021d7af67236aba2271eec35864c9889
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-03-29 12:40:37 +0200

    api models and some constants added

---
+++ app/src/main/java/com/seif/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.seif.foody
+
+class Constants {
+    companion object{
+        const val API_KEY = "e05e5350af934562a3450155e352ab04"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
