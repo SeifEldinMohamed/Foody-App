4c2fe2bbe9f208fa7babfb9567e906335594f441
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-06 00:00:08 +0200

    add viewpager adapter to tabLayout

--- app/src/main/java/com/seif/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/seif/foody/ui/DetailsActivity.kt
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navArgs
 import com.seif.foody.R
+import com.seif.foody.adapters.PagerAdapter
 import com.seif.foody.databinding.ActivityDetailsBinding
+import com.seif.foody.ui.fragments.ingredients.IngredientsFragment
+import com.seif.foody.ui.fragments.instructions.InstructionsFragment
+import com.seif.foody.ui.fragments.overview.OverviewFragment
 import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.activity_details.view.*
+import kotlinx.android.synthetic.main.placeholder_row_layout.*
 
 class DetailsActivity : AppCompatActivity() {
     lateinit var binding: ActivityDetailsBinding
+    private val args by navArgs<DetailsActivityArgs>()
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,6 +31,27 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val adpater = PagerAdapter(
+            resultBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+        )
+        binding.viewPager.adapter = adpater
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
 
     }
 
