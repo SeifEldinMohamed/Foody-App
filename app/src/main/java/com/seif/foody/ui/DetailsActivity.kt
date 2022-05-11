8b469d0a97f3f171cf937f098bd092d43d3198ff
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-11 16:41:10 +0200

    Create details menu

--- app/src/main/java/com/seif/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/seif/foody/ui/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.seif.foody.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -53,6 +54,11 @@ class DetailsActivity : AppCompatActivity() {
             tab.text = titles[position]
         }.attach()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.details_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
