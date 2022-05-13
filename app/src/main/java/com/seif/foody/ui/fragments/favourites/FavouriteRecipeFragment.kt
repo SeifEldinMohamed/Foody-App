8642a19f2bc6e0ae0102f1793cb6fb64e0b1be78
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-13 13:25:18 +0200

    Display Favourite Recipes

--- app/src/main/java/com/seif/foody/ui/fragments/favourites/FavouriteRecipeFragment.kt
+++ app/src/main/java/com/seif/foody/ui/fragments/favourites/FavouriteRecipeFragment.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.seif.foody.R
+import com.seif.foody.adapters.FavouriteRecipeAdapter
+import com.seif.foody.databinding.FragmentFavouriteReciepeBinding
+import com.seif.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class FavouriteRecipeFragment : Fragment() {
-
-
+    private lateinit var binding:FragmentFavouriteReciepeBinding
+    private val favouriteRecipeAdapter: FavouriteRecipeAdapter by lazy { FavouriteRecipeAdapter() }
+    private val mainViewModel : MainViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite_reciepe, container, false)
+        binding = FragmentFavouriteReciepeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainViewModel.readFavouriteRecipes.observe(viewLifecycleOwner){ favouriteRecipes ->
+            favouriteRecipeAdapter.addFavouriteRecipes(favouriteRecipes)
+        }
+
+        binding.rvFavouriteRecipe.adapter = favouriteRecipeAdapter
+
+
+    }
 
 }
