d1d0331403ae85bc66144bb61d1efd5b5ae78c2e
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-20 14:34:08 +0200

    Food Joke Binding Class part 1

---
+++ app/src/main/java/com/seif/foody/bindingadapters/FoodJokeBinding.kt
@@ -0,0 +1,53 @@
+package com.seif.foody.bindingadapters
+
+import android.view.View
+import android.widget.ProgressBar
+import androidx.cardview.widget.CardView
+import androidx.databinding.BindingAdapter
+import com.google.android.material.card.MaterialCardView
+import com.seif.foody.data.database.entities.FoodJokeEntity
+import com.seif.foody.models.FoodJoke
+import com.seif.foody.utils.NetworkResult
+import retrofit2.Response
+
+class FoodJokeBinding {
+    companion object {
+
+        @BindingAdapter("readApiResponse3", "readDatabase3", requireAll = false)
+        @JvmStatic
+        fun setCardAndProgressBarVisibility(
+            view: View,
+            apiResponse: NetworkResult<FoodJoke>?,
+            database: List<FoodJokeEntity>?
+        ) {
+            when (apiResponse) {
+                is NetworkResult.Loading -> {
+                    when (view) {
+                        is ProgressBar -> view.visibility = View.VISIBLE
+                        is MaterialCardView -> view.visibility = View.INVISIBLE
+                    }
+                }
+                is NetworkResult.Error -> {
+                    when (view) {
+                        is ProgressBar -> view.visibility = View.INVISIBLE
+                        is MaterialCardView -> {
+                            view.visibility = View.VISIBLE
+                            if (database != null) {
+                                if (database.isEmpty()) {
+                                    view.visibility = View.INVISIBLE
+                                }
+                            }
+                        }
+                    }
+                }
+                is NetworkResult.Success -> {
+                    when (view) {
+                        is ProgressBar -> view.visibility = View.INVISIBLE
+                        is MaterialCardView -> view.visibility = View.VISIBLE
+                    }
+                }
+
+            }
+        }
+    }
+}
