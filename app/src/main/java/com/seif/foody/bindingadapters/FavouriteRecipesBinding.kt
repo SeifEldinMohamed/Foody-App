99de8ebe3737b10441f12a51171129ee26c1cb0a
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-26 15:41:24 +0200

    FavouriteRecipesBinding Code Clean Up

--- app/src/main/java/com/seif/foody/bindingadapters/FavouriteRecipesBinding.kt
+++ app/src/main/java/com/seif/foody/bindingadapters/FavouriteRecipesBinding.kt
@@ -3,6 +3,7 @@ package com.seif.foody.bindingadapters
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.seif.foody.adapters.FavouriteRecipeAdapter
@@ -10,42 +11,27 @@ import com.seif.foody.data.database.entities.FavouriteEntity
 
 class FavouriteRecipesBinding {
     companion object {
-        // viewVisibility refers to favouriteRecipe
+        // setVisibility refers to favouriteRecipe
         // setData refers to myAdapter
 
-        @BindingAdapter("viewVisibility","setData", requireAll = false)
+        @BindingAdapter("setVisibility", "setData", requireAll = false)
         @JvmStatic
-        fun setDataAndViewVisibility(
+        fun setVisibility(
             view: View,
             favouriteRecipe: List<FavouriteEntity>?,
             myAdapter: FavouriteRecipeAdapter?
         ) {
-            if(favouriteRecipe.isNullOrEmpty()){ // no favourite recipes
-                when(view){
-                    is TextView -> {
-                        view.visibility = View.VISIBLE
-                    }
-                    is ImageView -> {
-                        view.visibility = View.VISIBLE
-                    }
-                    is RecyclerView -> {
-                        view.visibility = View.INVISIBLE
+            when (view) {
+                is RecyclerView -> {
+                    val dataCheck = favouriteRecipe.isNullOrEmpty()
+                    view.isVisible = dataCheck
+                    if (!dataCheck) {
+                        favouriteRecipe?.let {
+                            myAdapter?.addFavouriteRecipes(it)
+                        }
                     }
                 }
-            }
-            else{
-                when(view){
-                    is TextView -> {
-                        view.visibility = View.INVISIBLE
-                    }
-                    is ImageView -> {
-                        view.visibility = View.INVISIBLE
-                    }
-                    is RecyclerView -> {
-                        view.visibility = View.VISIBLE
-                        myAdapter?.addFavouriteRecipes(favouriteRecipe)
-                    }
-                }
+                else -> view.isVisible = favouriteRecipe.isNullOrEmpty()
             }
         }
     }
